5214c4746bdf15c365813592af6ad9c820fd17ec
nitishshar <nitishshar@gmail.com>
2024-10-14 17:13:33 -0400

    Chnaged the style of cards

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FleetDataService } from '../services/fleet-management.service';
-import { NgFor, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatCardModule, MatToolbarModule,NgFor, NgStyle],
+  imports: [MatCardModule, MatToolbarModule, NgFor, NgStyle, NgClass],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-  public  fleetData:any; 
+  public fleetData: any;
 
   constructor(private fleetDataService: FleetDataService) {
     this.fleetData = this.fleetDataService.getFleetData(); // Signal for fleet data
@@ -20,21 +20,11 @@ export class LandingPageComponent {
 
   ngOnInit() {}
 
-   // Utility function to darken a color slightly
-   public darkenColor(color: string): string {
-    let num = parseInt(color.replace('#', ''), 16),
-        amt = -30,
-        R = (num >> 16) + amt,
-        G = ((num >> 8) & 0x00FF) + amt,
-        B = (num & 0x0000FF) + amt;
-  
-    return '#' + (
-      0x1000000 + 
-      (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 + 
-      (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 + 
-      (B < 255 ? (B < 1 ? 0 : B) : 255)
-    ).toString(16).slice(1);
+  // Utility function to generate random number
+  public random(): number {
+    return Math.floor(Math.random() * 5) + 1;
   }
-  
 }
 // background-color: rgb(17,45,67);
+//background-color: #8bc6ec;
+//background-image: linear-gradient(135deg, #8bc6ec 0%, #9599e2 100%);
